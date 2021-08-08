ccd01f0b8a9eb7be21101d03d6b169c6a5d57279
Irina Baron <irr.baron@gmail.com>
2021-08-08 18:51:11 +0300

    worked with header section

--- js/script.js
+++ js/script.js
@@ -29,27 +29,27 @@ $(document).ready(function () {
 });
 
 let myFunction = document.querySelector('#myFunction');
-let headerLinks = document.querySelectorAll('.menu-item-click');
+let headerLinks = document.querySelectorAll('.menu-item__click');
 let linksDrop = document.querySelectorAll('.menu-link__drop')
 
 for(let headerLink of headerLinks) {
   headerLink.addEventListener('click', function () {
-    // document.getElementById("myDrop1").classList.toggle("show");
+  //   document.getElementById("myDrop1").classList.toggle("show");
   // let elem;
-    // document.getElementById('myDrop2').classList.remove('show');
-    // document.getElementById('myDrop3').classList.remove('show');
-    // document.getElementById('myDrop4').classList.remove('show');
-    // document.getElementById('myDrop5').classList.remove('show');
-    for(let i = 0; i < linksDrop.length; i++) {
-      elem = linksDrop[i];
-      console.log(elem)
-      if(!elem.classList.contains('show')) {
-        elem.classList.add('show');
-      } else {
-        elem.classList.remove('show');
-      }
+  //   document.getElementById('myDrop2').classList.remove('show');
+  //   document.getElementById('myDrop3').classList.remove('show');
+  //   document.getElementById('myDrop4').classList.remove('show');
+  //   document.getElementById('myDrop5').classList.remove('show');
+    // for(let i = 0; i < linksDrop.length; i++) {
+    //   elem = linksDrop[i];
+    //   console.log(elem)
+    //   if(!elem.classList.contains('show')) {
+    //     elem.classList.add('show');
+    //   } else {
+    //     elem.classList.remove('show');
+    //   }
       
-    }
+    // }
 
     // for(let linkDrop of linksDrop) {
     //   let elem = document.getElementsByClassName('show');
@@ -64,18 +64,18 @@ for(let headerLink of headerLinks) {
   })
 }
 
-// myFunction.addEventListener('click', function () {
-//   document.getElementById("myDrop1").classList.toggle("show");
+myFunction.addEventListener('click', function () {
+  document.getElementById("myDrop1").classList.toggle("show");
   
-//   document.getElementById('myDrop2').classList.remove('show');
-//   document.getElementById('myDrop3').classList.remove('show');
-//   document.getElementById('myDrop4').classList.remove('show');
-//   document.getElementById('myDrop5').classList.remove('show');
-// })
+  document.getElementById('myDrop2').classList.remove('show');
+  document.getElementById('myDrop3').classList.remove('show');
+  document.getElementById('myDrop4').classList.remove('show');
+  document.getElementById('myDrop5').classList.remove('show');
+})
 
 window.onclick = function(event) {
   
-  if (!event.target.matches('.menu-item-click')) {
+  if (!event.target.matches('.menu-item__click')) {
     var dropdowns = document.getElementsByClassName("drop-list");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -87,41 +87,41 @@ window.onclick = function(event) {
   }  
 };
 
-// document.querySelector('#my_Drop2').addEventListener('click', function () {
-//   document.getElementById("myDrop2").classList.toggle("show");
+document.querySelector('#my_Drop2').addEventListener('click', function () {
+  document.getElementById("myDrop2").classList.toggle("show");
 
-//   document.getElementById('myDrop1').classList.remove('show');
-//   document.getElementById('myDrop3').classList.remove('show');
-//   document.getElementById('myDrop4').classList.remove('show');
-//   document.getElementById('myDrop5').classList.remove('show');
-// });
+  document.getElementById('myDrop1').classList.remove('show');
+  document.getElementById('myDrop3').classList.remove('show');
+  document.getElementById('myDrop4').classList.remove('show');
+  document.getElementById('myDrop5').classList.remove('show');
+});
 
-// document.querySelector('#my_Drop3').addEventListener('click', function () {
-//   document.getElementById("myDrop3").classList.toggle("show");
+document.querySelector('#my_Drop3').addEventListener('click', function () {
+  document.getElementById("myDrop3").classList.toggle("show");
   
-//   document.getElementById('myDrop2').classList.remove('show');
-//   document.getElementById('myDrop1').classList.remove('show');
-//   document.getElementById('myDrop4').classList.remove('show');
-//   document.getElementById('myDrop5').classList.remove('show');
-// })
+  document.getElementById('myDrop2').classList.remove('show');
+  document.getElementById('myDrop1').classList.remove('show');
+  document.getElementById('myDrop4').classList.remove('show');
+  document.getElementById('myDrop5').classList.remove('show');
+})
 
-// document.querySelector('#my_Drop4').addEventListener('click', function () {
-//   document.getElementById("myDrop4").classList.toggle("show");
+document.querySelector('#my_Drop4').addEventListener('click', function () {
+  document.getElementById("myDrop4").classList.toggle("show");
 
-//   document.getElementById('myDrop2').classList.remove('show');
-//   document.getElementById('myDrop3').classList.remove('show');
-//   document.getElementById('myDrop1').classList.remove('show');
-//   document.getElementById('myDrop5').classList.remove('show');
-// })
+  document.getElementById('myDrop2').classList.remove('show');
+  document.getElementById('myDrop3').classList.remove('show');
+  document.getElementById('myDrop1').classList.remove('show');
+  document.getElementById('myDrop5').classList.remove('show');
+})
 
-// document.querySelector('#my_Drop5').addEventListener('click', function () {
-//   document.getElementById("myDrop5").classList.toggle("show");
+document.querySelector('#my_Drop5').addEventListener('click', function () {
+  document.getElementById("myDrop5").classList.toggle("show");
 
-//   document.getElementById('myDrop2').classList.remove('show');
-//   document.getElementById('myDrop3').classList.remove('show');
-//   document.getElementById('myDrop4').classList.remove('show');
-//   document.getElementById('myDrop1').classList.remove('show');
-// })
+  document.getElementById('myDrop2').classList.remove('show');
+  document.getElementById('myDrop3').classList.remove('show');
+  document.getElementById('myDrop4').classList.remove('show');
+  document.getElementById('myDrop1').classList.remove('show');
+})
 
 
 
